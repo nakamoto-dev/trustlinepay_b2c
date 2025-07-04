ac9cfd64b337820014028aca1c8b05763f66d1a1
Dennis Chumba <85931657+nakamoto-dev@users.noreply.github.com>
2025-07-04 16:09:10 +0300

    Update route.php

--- laravel/complete_example/route.php
+++ laravel/complete_example/route.php
@@ -1,6 +1,8 @@
+<?php
+
 // routes/web.php
 
-use App\Http\Controllers\Examples\TrustlinePageController;
+use App\Http\Controllers\TrustlinePageController;
 
 Route::get('/trustline-demo', [TrustlinePageController::class, 'form']);
 Route::post('/trustline-demo/send', [TrustlinePageController::class, 'send'])->name('trustline.send');
